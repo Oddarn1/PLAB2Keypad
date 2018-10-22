70a6c0ff147ffa67cd258fb25a66ba40ca222f56
Odd Andre <oddandreowren@gmail.com>
2018-10-22 17:10:45 +0200

    Add passcode.txt and updated KPC

--- KPC.py
+++ KPC.py
@@ -1,1 +1,15 @@
+class KPC():
 
+
+    def __init__(self,keypad,ledBoard):
+        self.keypad=keypad
+        self.ledBoard=ledBoard
+        self.__pass_file=
+
+    def init_passcode_entry(self):
+        #Power up and clear passcode-buffer
+
+    def get_next_signal(self):
+        #return override-signal, wait for keypress
+
+    def verify_login(self):
