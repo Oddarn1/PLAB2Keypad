82e27eddb8240cd0471b600dce52a542a1b648f7
Odd Andre <oddandreowren@gmail.com>
2018-10-22 17:18:08 +0200

    Add function verify passcode

--- KPC.py
+++ KPC.py
@@ -4,7 +4,7 @@ class KPC():
     def __init__(self,keypad,ledBoard):
         self.keypad=keypad
         self.ledBoard=ledBoard
-        self.__pass_file=
+        self.__pass_file="passcode.txt"
 
     def init_passcode_entry(self):
         #Power up and clear passcode-buffer
@@ -12,4 +12,5 @@ class KPC():
     def get_next_signal(self):
         #return override-signal, wait for keypress
 
-    def verify_login(self):
+    def verify_login(self,loginText):
+        return open(self.__pass_file).readline()
