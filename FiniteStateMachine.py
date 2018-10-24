d67c043af3e774bef4c2fdeb80d13efd124c56e7
haakofli <hakon.fuhre.lia@gmail.com>
2018-10-24 16:31:05 +0200

    Final commit

--- FiniteStateMachine.py
+++ FiniteStateMachine.py
@@ -2,11 +2,11 @@ from inspect import isfunction
 import re
 import inspect
 from LedBoard import LedBoard
-from Keypad import keypad
+from Keypad import Keypad
 from KPC import KPC
 
-class FSM:
 
+class FSM:
 
     def __init__(self, agent):
         self.rules = []
@@ -16,11 +16,6 @@ class FSM:
     def add_rule(self,rule):
         # add a new rule to the end of the FSM's rule list
         self.rules.append(rule)
-
-    def create_all_rules(self):
-        def signal_is_digit(signal):return 48<=ord(signal)<=57
-        def signal_is_asterisk(signal):return signal=='*'
-
 
     def get_next_signal(self):
         # query the agent for the next signal
@@ -58,15 +53,6 @@ class FSM:
         else:
             return False
 
-    def main_loop(self):
-        #Loop through and change states until FSM reaches full state
-        #Create rules
-        self.keypad=keypad()
-        self.ledboard=LedBoard()
-        self.keypad.setup()
-        self.ledboard.setup()
-        self.agent=KPC(self.keypad,self.ledboard)
-        currentState=0
 
     def make_all_rules(self):           # Need to put in symbols
 
@@ -97,17 +83,37 @@ class FSM:
         logout_rule = Rule("S_logout", "S_done", "#", self.agent.exit_action)
         self.add_rule(logout_rule)
 
-    def all_symbols(self, sym):
-        if sym.isdigit() or sym == "#" or sym == "*": return True
-        else: return False
+    @staticmethod
+    def all_symbols(sym):
+        if sym.isdigit() or sym == "#" or sym == "*":
+            return True
+        else:
+            return False
 
-    def signal_is_digit(self, sym): return 48 <= ord(sym) <= 57
+    @staticmethod
+    def signal_is_digit(sym):
+        return 48 <= ord(sym) <= 57
 
 
 class Rule:
-    def __init__(self, current_state, next_state, signal, action):
-        # self.current_state = current_state                            # Trenger egt ikke denne
+    def __init__(self,current_state,next_state, signal, action):
         self.next_state = next_state
         self.signal = signal
         self.action = action
 
+
+def main_loop():
+    kp=Keypad()
+    lb=LedBoard()
+    agent = KPC(kp,lb)
+    fsm = FSM(agent)
+    fsm.make_all_rules()
+    while fsm.current_state != "S_done":
+        signal = fsm.get_next_signal()
+        print(signal)
+        fsm.run_rules(signal)
+        print(fsm.current_state)
+
+
+if __name__ == '__main__':
+    main_loop()
